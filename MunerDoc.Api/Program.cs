52bc186957d442e0bb9bab426aa218eb9c25853b
lybitechokoladok <102763296+lybitechokoladok@users.noreply.github.com>
2024-06-10 03:52:47 +0300

    add primitives at domain project

--- MunerDoc.Api/Program.cs
+++ MunerDoc.Api/Program.cs
@@ -1,11 +1,18 @@
+using MunerDoc.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+    builder => 
+    {
+        builder.WithOrigins("http://localhost:3000");
+    }));
 
 var app = builder.Build();
 
